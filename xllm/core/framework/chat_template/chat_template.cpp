0fd1c9a21bf77e5c72bcf25e34eb121cc5e602db
pengtao <pengtao.156@jd.com>
2026-04-28 17:22:16 +0800

    feat: support c++ chat template for deepseek_v4 model.

--- xllm/core/framework/chat_template/chat_template.cpp
+++ xllm/core/framework/chat_template/chat_template.cpp
@@ -19,6 +19,7 @@ limitations under the License.
 #include <glog/logging.h>
 
 #include "framework/chat_template/deepseek_v32_cpp_template.h"
+#include "framework/chat_template/deepseek_v4_cpp_template.h"
 #include "framework/chat_template/jinja_chat_template.h"
 
 DECLARE_bool(use_cpp_chat_template);
@@ -28,10 +29,19 @@ namespace xllm {
 std::unique_ptr<ChatTemplate> ChatTemplate::create(
     const TokenizerArgs& tokenizer_args,
     const std::string& model_type) {
-  if (FLAGS_use_cpp_chat_template && model_type == "deepseek_v32") {
-    LOG(INFO) << "Using native C++ chat template for "
-              << "model_type: " << model_type;
-    return std::make_unique<DeepseekV32CppTemplate>(tokenizer_args);
+  if (FLAGS_use_cpp_chat_template) {
+    if (model_type == "deepseek_v32") {
+      LOG(INFO) << "Using native C++ chat template for "
+                << "model_type: " << model_type;
+      return std::make_unique<DeepseekV32CppTemplate>(tokenizer_args);
+    } else if (model_type == "deepseek_v4") {
+      LOG(INFO) << "Using native C++ chat template for "
+                << "model_type: " << model_type;
+      return std::make_unique<DeepseekV4CppTemplate>(tokenizer_args);
+    } else {
+      LOG(FATAL) << "cpp_chat_template only support deepseekv32 and deepseekv4 "
+                    "models currently.";
+    }
   }
   LOG(INFO) << "Using Jinja chat template for "
             << "model_type: " << model_type;
